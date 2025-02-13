404c6734968a1c924cafcab416f04ddcefeecd06
ranjithyl <51478932+ranjithyl@users.noreply.github.com>
2025-02-13 20:36:06 +0530

    Update SendEmail.ps1

--- SendEmail.ps1
+++ SendEmail.ps1
@@ -2,7 +2,7 @@ param (
     [string]$Recipient = "ranjithyl95@yahoo.com"
 )
 
-$smtpServer = "smtp.gmail.com"
+$smtpServer = "smtp.mail.yahoo.com"
 $from = "noreply@example.com"
 
 $subject = "Windows Server Validation Report"
