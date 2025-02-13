79bd2fd0e59350935b3628bc69f4eddef104f5c1
ranjithyl <51478932+ranjithyl@users.noreply.github.com>
2025-02-13 20:52:58 +0530

    Update SendEmail.ps1

--- SendEmail.ps1
+++ SendEmail.ps1
@@ -8,6 +8,6 @@ $from = "noreply@example.com"
 $subject = "Windows Server Validation Report"
 $body = "Attached are the latest validation reports for the servers after patching."
 
-$attachments = @("PingResults.csv", "RDPStatus.csv", "LastReboot.csv", "RDPEventLog.csv")
+$attachments = @("PingResults.csv")     #, "RDPStatus.csv", "LastReboot.csv", "RDPEventLog.csv")
 
 Send-MailMessage -To $Recipient -From $from -Subject $subject -Body $body -SmtpServer $smtpServer -Attachments $attachments
